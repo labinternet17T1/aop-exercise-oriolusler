72fd42cb7a0107bf98e6bd4d698a58824982d395
Oriol Soler <oriol.soler.cabot@hotmail.com>
2017-10-13 22:02:12 +0200

    classe contorller i loggerAdvice

---
+++ src/main/java/cat/tecnocampus/aop/LoggerAdvice.java
@@ -0,0 +1,61 @@
+package cat.tecnocampus.aop;
+
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import cat.tecnocampus.domain.Classroom;
+import org.aspectj.lang.ProceedingJoinPoint;
+
+import org.aspectj.lang.annotation.*;
+
+import org.slf4j.Logger;
+@Aspect
+@Component
+public class LoggerAdvice {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggerAdvice.class);
+
+    @Pointcut("execution(* cat.tecnocampus.persintence.Controller.*(..)) && args (Class)")
+
+    public void pointcutClassroom() {logger.info("Going to get a classroom method");}
+
+    @Before("pointcutClassroom()")
+    public void beforeit(){}
+
+
+
+
+
+
+    @Pointcut("execution(* cat.tecnocampus.persintence.Controller.find*(..))")
+
+    public void pointcutFind() {logger.info("Going to show a method with find word");}
+
+    @After("pointcutFind()")
+    public void afterIt(){logger.info("After a method with 'find' word");}
+
+
+
+
+
+    @Pointcut("execution(* cat.tecnocampus.persintence.Controller.insertBatch(..))")
+
+    public void pointcutBach() {logger.info("Insterting a batch");}
+    @Around("pointcutBach()")
+
+    public void pointBatch(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        logger.info("before multiple insert " + joinPoint);
+
+        if (true) {
+
+            joinPoint.proceed(); System.out.println("insertBatch");
+
+        }
+
+        logger.info("after multiple insert " + joinPoint);
+
+    }
+
+}
---
+++ src/main/java/cat/tecnocampus/persintence/Controller.java
@@ -0,0 +1,53 @@
+package cat.tecnocampus.persintence;
+
+import cat.tecnocampus.domain.Classroom;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("Controller")
+public class Controller {
+
+        // private static Controller controller;
+        private ClassroomDAO classroomDAO;
+
+        public Controller(ClassroomDAO classroomDAO){
+            this.classroomDAO  = classroomDAO;
+        }
+
+        /*  public synchronized static Controller getInstace(){
+              if(controller == null)
+                  controller = new Controller();
+              return controller;
+          }
+      */
+        public List<Classroom> findAll(){
+            return classroomDAO.findAll();
+        }
+
+        public List<Classroom> findCapacityLargerThan(int capacity){
+            return classroomDAO.findCapacityLargerThan(capacity);
+        }
+
+        public List<Classroom> findCapacityLowerThan(int capacity){
+            return classroomDAO.findCapacityLowerThan(capacity);
+        }
+
+        public List<Classroom> findWithPlugs(){
+            return  classroomDAO.findWithPlugs();
+        }
+
+        public  List<Classroom> findWithNoPlugs(){
+            return classroomDAO.findWithNoPlugs();
+        }
+
+        public int insert(Classroom classroom){
+            return classroomDAO.insert(classroom);
+        }
+
+        public int[] insertBatch(List<Classroom> classrooms){
+            return classroomDAO.insertBatch(classrooms);
+        }
+
+    }
+
